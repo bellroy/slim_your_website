1e6b2228989dee586f99e01a70ecdab28710eb47
Michael Webb <moggyboy@gmail.com>
2015-06-17 13:16:01 +1000

    First commit.

---
+++ README.md
@@ -0,0 +1,40 @@
+# Slim Your Website
+
+This is a command line utility that runs multiple requests to a list of URLS and gives you information about how long they take to download, how big they are, how many files get requested, etc. etc.
+
+## Command line usage
+
+You'll need a text file containing a list of URLs you want to test.
+
+```
+ruby slim_your_wallet.rb input_file.txt 
+```
+By default, the utility will run 10 requests against all valid URLs in the file and give you some output, like this:
+
+```
+HTTP://GOOGLE.COM
+Realtime download times (s): [2.3, 1.5, 1.9, 1.4, 1.4]
+Average realtime download time (s): 1.7
+Files downloaded: 5
+Download size (KB): 70
+CPU work times (s): [0.2, 0.2, 0.3, 0.1, 0.2]
+Average CPU work time (s): 0.2
+Connection speeds (KB/s): [353, 438, 207, 472, 365]
+Average connection speed (KB/s): 367
+
+HTTP://TRIKEAPPS.COM
+Realtime download times (s): [18.0, 19.0, 17.0, 16.0, 15.0]
+Average realtime download time (s): 17.0
+Files downloaded: 26
+Download size (KB): 203
+CPU work times (s): [1.3, 1.6, 1.6, 1.4, 1.5]
+Average CPU work time (s): 1.5
+Connection speeds (KB/s): [162, 130, 129, 145, 137]
+Average connection speed (KB/s): 140
+```
+
+If you want to increase or reduce the number of requests, use the second argument on the command line:
+
+```
+ruby slim_your_wallet.rb input_file.txt 3
+```
---
+++ slim_your_website.rb
@@ -0,0 +1,115 @@
+require 'uri'
+require 'fileutils'
+
+class SlimYourWebsite
+  DOWNLOAD_REPEATS = 10
+  DOWNLOAD_TIME_PREFIX = "Total wall clock time: "
+  PAGE_SIZE_PREFIX = "Downloaded: "
+  OUTPUT_FILENAME = "output.txt"
+
+  def assess(filename, no_of_times = DOWNLOAD_REPEATS)
+    lines = IO.readlines(filename).map(&:strip)
+    self.current_request = 0
+    self.total_requests = lines.length * no_of_times
+    result = ""
+    lines.each do |url|
+      if is_valid_url?(url)
+        output = get_site_repeatedly(url, no_of_times || DOWNLOAD_REPEATS)
+        result += massage_output(url, output)
+        cleanup_output_file
+      end
+    end if File.exists?(ARGV[0])
+    result
+  end
+
+  private
+
+  attr_accessor :total_requests, :current_request
+
+  def is_valid_url?(url)
+    url =~ URI::regexp
+  end
+
+  def update_progress
+    self.current_request += 1
+    percent = ((current_request / total_requests.to_f) * 100).to_i
+    print "\r"
+    print "#{current_request}/#{total_requests} requests completed - #{percent}% complete"
+  end
+
+  def get_site_repeatedly(url, no_of_times)
+    output = []
+    FileUtils.mkdir("output") rescue Errno::EEXIST
+    no_of_times.times do
+      command = "cd output; wget -E -H -k -p -nv --output-file=../#{OUTPUT_FILENAME} #{url}; cd ../; cat #{OUTPUT_FILENAME}"
+      output << `#{command}`
+      update_progress
+    end
+    FileUtils.remove_dir("output")
+    output
+  end
+
+  def cleanup_output_file
+    File.delete(OUTPUT_FILENAME)
+  end
+
+  def massage_output(url, output)
+    massaged_output = [url.upcase + " " * 40]
+    massaged_output << collect_realtime_information(output)
+    massaged_output << collect_size_and_cpu_information(output)
+    massaged_output << " "
+    massaged_output.join("\n") + "\n"
+  end
+
+  def collect_realtime_information(output)
+    times = []
+    output.each do |output_entry|
+      lines = output_entry.split("\n")
+      line = lines.find{|line| line.start_with?(DOWNLOAD_TIME_PREFIX)}
+      if line
+        match_data = /#{DOWNLOAD_TIME_PREFIX}(.*)s/.match(line)
+        times << match_data[1].gsub(",", "").to_f
+      end
+    end
+    return "Realtime download times (s): #{times}\n" +
+      "Average realtime download time (s): #{average(times)}"
+  end
+
+  def collect_size_and_cpu_information(output)
+    files_downloaded = nil
+    download_size = nil
+    times = []
+    connection_speeds = []
+    output.each do |output_entry|
+      lines = output_entry.split("\n")
+      line = lines.find{|line| line.start_with?(PAGE_SIZE_PREFIX)}
+      if line
+        match_data = /#{PAGE_SIZE_PREFIX}([0-9]+) files, (.*)K in (.*)s \((.*) KB\/s\)/.match(line)
+        files_downloaded = match_data[1].gsub(",", "").to_i
+        download_size = match_data[2].gsub(",", "").to_i
+        times << match_data[3].gsub(",", "").to_f
+        connection_speeds << match_data[4].gsub(",", "").to_i
+      end
+    end
+    return "Files downloaded: #{files_downloaded}\n" +
+      "Download size (KB): #{download_size}\n" +
+      "CPU work times (s): #{times}\n" +
+      "Average CPU work time (s): #{average(times)}\n" +
+      "Connection speeds (KB/s): #{connection_speeds}\n" +
+      "Average connection speed (KB/s): #{average(connection_speeds).to_i}"
+  end
+
+  def average(array_of_numbers)
+    sum = array_of_numbers.inject(&:+)
+    return (sum / array_of_numbers.length).round(1)
+  end
+end
+
+slim_your_website = SlimYourWebsite.new
+times = ARGV[1].to_i
+filename = ARGV[0]
+result = slim_your_website.assess(filename, times)
+print "\r"
+puts result
+
+
